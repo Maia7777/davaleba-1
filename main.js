f93bfbf61126fa8ca02afc9a4c5711d37a574da5
Maia7777 <mayatavadze4@gmail.com>
2024-10-13 19:10:01 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+let num = 100;
+if (num < 50) {
+  console.log("ნაკლებია 50 ზე");
+} else if (num > 20) {
+  console.log("მეტია 20 ზე");
+} else {
+  console.log("შეცდომა");
+}
+//2
+let useName = "მარიამ";
+if (useName === "მარიამ") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+//switch case
+let UserName = "მარიამ";
+switch (UserName) {
+  case "მარიამ":
+    console.log(true);
+    break;                                                                                                                             
+ default:
+                                                                                              
+console.log(false);
+                                                                                              
+ break;
+                                                                                              
+  }
